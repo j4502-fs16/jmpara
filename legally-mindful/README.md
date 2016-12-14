a5ec60d8eb18826b17e18774ac6c671d2711943d
Jen Para <jmpxt4@mail.missouri.edu>
2016-12-14 14:43:36 -0600

    ADDed legally mindulf files?

---
+++ legally-mindful/README.md
@@ -0,0 +1,3 @@
+# legally-mindful
+Mindfulness in Law Society group repository
+https://j4502-fs16.github.io/legally-mindful/
---
+++ legally-mindful/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(600);
+});
+
+$(window).resize(function(){
+if (   $(window).width() > 600 ){
+  $('nav ul').removeAttr('style');
+}
+});
+});
